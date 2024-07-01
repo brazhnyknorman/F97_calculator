55645b129d54b8c6d809ff67e08ef34674a0fabc
brazhnyknorman <brazhnyknorman@gmail.com>
2024-07-01 16:14:07 -0400

    Add event listeners to 1-9, operator and clear buttons

--- javascript.js
+++ javascript.js
@@ -4,7 +4,70 @@ let operator = null;
 
 const body = document.querySelector("body");
 
-let userDisplay = [];
+const displayScreen = document.querySelector(".display");
+const initialDisplay = "Please Enter A Calculation";
+displayScreen.textContent = initialDisplay;
+
+const buttons = addEventsToButtons();
+
+let displayText = [];
+
+/* EVENT LISTENERS */
+
+displayScreen.addEventListener('click', function(onClick) {
+
+});
+
+
+
+/* FUNCTIONS */
+
+function addEventsToButtons() {
+  let result = [
+    'btn1', 'btn2', 'btn3',
+    'btn4', 'btn5', 'btn6', 
+    'btn7', 'btn8', 'btn9',
+    'add', 'subtract', 'multiply', 'divide', 'equals',
+    'clear'
+  ];
+
+  for (let i = 0; i <= 8; i++) {
+    result[i] = document.querySelector(`#btn${i+1}`);
+  
+    result[i].addEventListener('click', function(onClick) {
+      console.log(i+1);
+    });
+  }
+
+  for (let i = 9; i <= 14; i++) {
+    let temp = result[i];
+    result[i] = document.querySelector(`#${result[i]}`)
+
+    result[i].addEventListener('click', function(onClick) {
+      console.log(temp);
+    })
+  }
+
+
+
+  /*
+
+  let calculatorFunctions = ['+', '-', 'x', '÷', '=', 'clear'];
+  
+  for (let i = 1; i <= 9; i++) {
+    buttons[i] = document.querySelector(`#btn${i}`);
+  
+    buttons[i].addEventListener('click', function(onClick) {
+      console.log(i);
+    });
+  }
+
+  calculatorFunctions.forEach(button => {
+    
+  });
+  */
+  return result;
+}
 
 function operate(a, operator, b) {
   function add(a, b) {
